9ec17a7ca8cedc8b7c1c170be19f5086f2a6a7b5
Adam Shelley <adam.shelley18@gmail.com>
2024-09-03 21:23:39 +0100

    Adding a goal section

---
+++ goal.js
@@ -0,0 +1,35 @@
+const focusInput = document.querySelector('#focus-input');
+const focusContainer = document.querySelector('.focus');
+
+const getLocalStorageGoal = () => {
+    const goal = JSON.parse(localStorage.getItem('goal'));
+
+    const today = new Date();
+    const isSameDay = goal &&
+        new Date(goal.date).getFullYear() === today.getFullYear() &&
+        new Date(goal.date).getMonth() === today.getMonth() &&
+        new Date(goal.date).getDate() === today.getDate();
+
+        console.log(isSameDay)
+    if (isSameDay) {
+        focusInput.value = goal.goal;
+        focusContainer.classList.add('no-border');
+    }
+}
+
+
+focusInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        const goal = e.target.value;
+
+        if (goal) {
+            localStorage.setItem('goal',JSON.stringify({
+                date: new Date(),
+                goal
+            }));
+            focusContainer.classList.add('no-border');
+        }
+    }
+})
+
+getLocalStorageGoal()
